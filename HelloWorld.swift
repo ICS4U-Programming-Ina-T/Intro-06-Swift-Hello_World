1d6ccc153d25f2b0658191499144a88148bd4f3b
Ubuntu <ubuntu@ip-172-31-90-152.ec2.internal>
2022-02-17 03:01:22 +0000

    Created HelloWorld.swift.

---
+++ HelloWorld.swift
@@ -0,0 +1,20 @@
+//
+
+//  HelloWorld.swift
+
+//
+
+//  Created by Ina Tolo
+
+//  Created on 2022-2-16
+
+//  Version 1.0
+
+//  Copyright (c) 2022 Ina Tolo. All rights reserved.
+
+//
+
+//  The Hello World! program implements an application that                               
+//  simply displays "Hello World!" to the standard output.
+
+print("Hello, World.")
